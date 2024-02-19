8802285867b4089ee75c8a62fb2e433a39427dc2
wxxd-fxrest <fxrest704@gmail.com>
2024-02-19 17:21:21 +0900

    Initial Commit

---
+++ UpDownGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 밀가루 on 2/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
