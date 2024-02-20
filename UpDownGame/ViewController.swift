087f18e1dbcfbe5c0f9efe40d274177662e1e71f
wxxd-fxrest <fxrest704@gmail.com>
2024-02-20 16:24:55 +0900

    UpDownGame END

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -10,23 +10,75 @@ import UIKit
 class ViewController: UIViewController {
     
     let image = #imageLiteral(resourceName: "slider_thumb")
+    var randomValue: Int = 0
+    var tryCount: Int = 0
     @IBOutlet weak var slider: UISlider!
-
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         slider.setThumbImage(image, for:.normal)
+        reset()
     }
 
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         print(sender.value)
+        let integerValue: Int = Int(sender.value)
+        sliderValueLabel.text = String(integerValue)
+    }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) {(action) in self.reset()}
+        
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func touchUpButton(_ sender: UIButton) {
         print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount = tryCount + 1
+        tryCountLabel.text = "\(tryCount)/5"
+        
+        if randomValue == hitValue {
+            showAlert(message: "동일")
+            reset()
+        } else if tryCount >= 5 {
+            showAlert(message: "너 졌어")
+            reset()
+        } else if randomValue > hitValue {
+            // 사용자가 선택한 값이 정해진 값과 다를 때 그 값에 따라 사용자가 입력한 값을 minimumValueLabel 또는 maximumValueLabel에 바인딩
+            slider.minimumValue = Float(hitValue)
+            minimumValueLabel.text = String(hitValue)
+        } else {
+            slider.maximumValue = Float(hitValue)
+            maximumValueLabel.text = String(hitValue)
+        }
     }
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
         print("touch up reset button")
+        reset()
+    }
+    
+    func reset() {
+        print("reset")
+        randomValue = Int.random(in: 0...30)
+        print(randomValue)
+        tryCount = 0
+        tryCountLabel.text = "0/5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
     }
 }
 
